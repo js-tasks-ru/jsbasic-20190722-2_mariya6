1a69991d6e8c402351515507c5625a852eaaf19a
M K <mystique4444@yandex.ru>
2019-08-01 02:56:25 +0300

    tasks 1-module v2.0

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,5 +1,3 @@
-import { isNull, isNumber } from "util";
-
 /**
  * Power
  * @param {number} m base
@@ -7,11 +5,13 @@ import { isNull, isNumber } from "util";
  * @returns {number}
  */
   function pow(m, n) {
-      if (!isNaN(m) && !isNaN(n)) {
-        if( n != 1) {
-            return m * pow(m, n-1);
-            } else {
-                return m;
-            }
-        } else alert ("Ошибка! Вводимые числа должны быть натуральные!");
+    if (!isNaN(m) && !isNaN(n)) {
+      if ( n !== 1) {
+        return m * pow(m, n - 1);
+        } else {
+          return m;
+        }
+    } else {
+      alert ('Ошибка! Вводимые числа должны быть натуральные!');
     }
+  }
