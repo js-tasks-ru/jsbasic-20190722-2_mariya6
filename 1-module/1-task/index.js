823d4095b6856a4f37d09039c7bcb68860cc3543
M K <mystique4444@yandex.ru>
2019-08-01 02:36:38 +0300

    tasks 1-module

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,9 +1,17 @@
+import { isNull, isNumber } from "util";
+
 /**
  * Power
  * @param {number} m base
  * @param {number} n index
  * @returns {number}
  */
-function pow(m, n) {
-
-}
+  function pow(m, n) {
+      if (!isNaN(m) && !isNaN(n)) {
+        if( n != 1) {
+            return m * pow(m, n-1);
+            } else {
+                return m;
+            }
+        } else alert ("Ошибка! Вводимые числа должны быть натуральные!");
+    }
--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,6 +10,13 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
+  if (name.indexOf(" ") >= 0) {
+        alert("Error! The name should not have spaces!");
+    } else if (name.length > 4) {
+        return name;
+    } else {
+        alert("Error! The name should be more than 4 chars!");
+    }
 }
 
 function sayHello() {
