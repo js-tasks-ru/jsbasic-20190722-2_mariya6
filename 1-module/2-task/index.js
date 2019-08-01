136b57ec5aec6a29af87050b37e182b1df59b5cc
Maria Kalliopina <mystique4444@yandex.ru>
2019-08-01 12:45:24 +0300

    upd task 2 v 2.0

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,9 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  return ((name.indexOf(' ') < 0) && (name.length >= 4));
+  if (name) {
+    return ((name.indexOf(' ') < 0) && (name.length >= 4));
+  } return false;
 }
 
 function sayHello() {
