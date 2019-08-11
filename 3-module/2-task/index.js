2078bd9376bed715a934c88d1a4387174c652417
M K <mystique4444@yandex.ru>
2019-08-11 23:31:39 +0300

    2 task v2.1

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,16 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  const reg = /(-?\d+.?\d+)/gi;
+  const arr = str.match(reg);
+  arr.forEach((el, i) => {
+    arr[i] = parseFloat(el);
+  });
+
+  const obj = {
+    min: Math.min(Math, c),
+    max: Math.max(Math, c),
+  };
+
+  return obj;
 }
