34a497e198ffd104598d37b6c8c62e0db0670883
M K <mystique4444@yandex.ru>
2019-08-12 19:42:14 +0300

    mod 3

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,15 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  const result = data.filter(user => user.age <= age).map(user => {
+    return {
+      name: user.name,
+      balance: user.balance,
+    }
+  });
+  let str = '';
+  result.forEach((el, i) => {
+    str = str + result[i].name + ' ' + result[i].balance + '\n'
+    })
+  return result;
 }
